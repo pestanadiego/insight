ab6354dee913db61bc94c7016167da1cc4e123e0
Diego <pestanadiegoalberto@gmail.com>
2021-11-08 13:50:17 -0400

    Added development branch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import './utils/firebaseConfig';
 
 ReactDOM.render(<App />, document.getElementById('root'));
