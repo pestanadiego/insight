6f5c6a16234453f62fb889c5df3d8077fc2921c8
Diego <pestanadiegoalberto@gmail.com>
2021-11-11 17:29:57 -0400

    Icon of checkmark

--- src/pages/UnderReviewPage.jsx
+++ src/pages/UnderReviewPage.jsx
@@ -1,6 +1,7 @@
 //Pagina de redireccionamiento al registrarse como especialista
 import styles from "./UnderReviewPage.module.css";
 import { useHistory } from 'react-router-dom';
+import checkmarkIcon from "../icons/checkmark_under_review.svg";
 
 function UnderReviewPage() {
     const history = useHistory();
@@ -13,8 +14,9 @@ function UnderReviewPage() {
         <>
         <div className={styles.page}>
           <div className={styles.underReviewContainer}>
+              <img className={styles.checkmark} src={checkmarkIcon} alt=""/>
               <h2 className={styles.underReviewTitle}>Hemos recibido tu información de registro</h2>
-              <h2 className={styles.underReviewMessage}>¡Te notificaremos por correo lo antes posible!</h2>
+              <p className={styles.underReviewMessage}>¡Te notificaremos por correo lo antes posible!</p>
               <button type='submit' className={styles.underReviewBtn} onClick={handleClick}>Volver</button>
           </div>
         </div>
