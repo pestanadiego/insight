87d6c7c3d1ef52ebc1cf23ec8519597ae55867b3
samueloso <samuel.cedeno@correo.unimet.edu.ve>
2021-11-18 01:35:49 -0400

    dashboar-responsive

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,10 +2,11 @@ import styles from "./Dashboard.module.css";
 import { useContext, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 import { Link, useHistory } from "react-router-dom";
+import arrows from "../../images/icons/arrow.svg";
 
 function Dashboard() {
   const { user } = useContext(UserContext);
-  const [showDash, setDash] = useState(true);
+  const [showDash, setDash] = useState(false);
 
   return (
     <div>
@@ -13,25 +14,44 @@ function Dashboard() {
         <div id={styles[showDash ? "dash" : ""]} className={styles.dashboard}>
           <ul>
             <li>
-              <Link to="/profile" className={styles.link}>
+              <Link
+                onClick={() => setDash(!showDash)}
+                to="/profile"
+                className={styles.link}
+              >
                 Mi Perfil
               </Link>
             </li>
             <li>
-              <Link to="/profile" className={styles.link}>
+              <Link
+                onClick={() => setDash(!showDash)}
+                to="/profile"
+                className={styles.link}
+              >
                 Citas
               </Link>
             </li>
             <li id={styles["arrow"]}>
-              <button onClick> menu </button>
+              <button onClick={() => setDash(!showDash)}>
+                {" "}
+                <img src={arrows} alt="arrows" />{" "}
+              </button>
             </li>
             <li>
-              <Link to="/profile" className={styles.link}>
+              <Link
+                onClick={() => setDash(!showDash)}
+                to="/profile"
+                className={styles.link}
+              >
                 Pacientes
               </Link>
             </li>
             <li>
-              <Link to="/profile" className={styles.link}>
+              <Link
+                onClick={() => setDash(!showDash)}
+                to="/profile"
+                className={styles.link}
+              >
                 Mensajes
               </Link>
             </li>
