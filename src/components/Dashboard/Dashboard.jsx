a9b7b60f326cc93f85abd4f772d722c1a5114b46
samueloso <samuel.cedeno@correo.unimet.edu.ve>
2021-11-18 00:18:30 -0400

    .

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,43 +1,43 @@
-import styles from './Dashboard.module.css';
-import { useContext } from 'react';
-import { UserContext } from '../../context/UserContext';
-import { Link, useHistory } from 'react-router-dom';
+import styles from "./Dashboard.module.css";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import { Link, useHistory } from "react-router-dom";
 
 function Dashboard() {
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
-    return(
-        <div>
-          {!!user ? (
-            <div className={styles.dashboard}>
-              <ul>
-                <li>
-                  <Link to="/profile" className={styles.link}>
-                    Mi Perfil
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/profile" className={styles.link}>
-                    Citas
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/profile" className={styles.link}>
-                    Pacientes
-                  </Link>
-                </li>
-                <li>
-                    <Link to="/profile" className={styles.link}>
-                      Mensajes
-                    </Link>
-                </li>
-              </ul>
-            </div>
-            ) : (
-              <div className={styles.nodisplay}></div>
-            )}
+  return (
+    <div>
+      {!!user ? (
+        <div className={styles.dashboard}>
+          <ul>
+            <li>
+              <Link to="/profile" className={styles.link}>
+                Mi Perfil
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile" className={styles.link}>
+                Citas
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile" className={styles.link}>
+                Pacientes
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile" className={styles.link}>
+                Mensajes
+              </Link>
+            </li>
+          </ul>
         </div>
-    )
+      ) : (
+        <div className={styles.nodisplay}></div>
+      )}
+    </div>
+  );
 }
 
 export default Dashboard;
