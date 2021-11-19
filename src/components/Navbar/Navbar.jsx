92bb9a5f165bf31736bba0331b170268512bfc43
samueloso <samuel.cedeno@correo.unimet.edu.ve>
2021-11-19 04:01:01 -0400

    navbar falta cerrarsesion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,10 @@ import { UserContext } from "../../context/UserContext";
 import { auth } from "../../utils/firebaseConfig";
 import styles from "./Navbar.module.css";
 import logo from "../../images/logo.svg";
-import menu_desp from "../../icons/menu_desp.svg";
+import homelogo from "../../icons/home_logo.svg";
+import loginLogo from "../../icons/login_logo.svg";
+import especialisLogo from "../../icons/especialist_logo.svg";
+import pacientLogo from "../../icons/paciente_logo.svg";
 
 function Navbar() {
   const history = useHistory();
@@ -19,67 +22,77 @@ function Navbar() {
   };
 
   return (
-    <ul className={styles.navbarContainer}>
-      <li>
-        <Link to="/" className={styles.link}>
-          <img className={styles.logo} src={logo} alt="logo" />
-        </Link>
-        <div className={styles.resboton}>
-          <button onClick={() => setMenu(!showMenu)} className={styles.login}>
-            <img className={styles.menu_desp} src={menu_desp} alt="menu_desp" />
-          </button>
-        </div>
-      </li>
+    <div className={styles.main_navContainer}>
+      <ul className={styles.navbarContainer}>
+        <li id={styles["logoshow"]}>
+          <Link to="/" className={styles.link}>
+            <img className={styles.logo} src={logo} alt="logo" />
+          </Link>
+        </li>
 
-      <li id={styles[showMenu ? "hidden" : ""]} className={styles.rightSide}>
-        {!!user ? (
-          <div className={styles.container}>
-            <button
-              type="button"
-              className={styles.logoutBtn}
-              onClick={handleLogout}
-            >
-              CERRAR SESIÓN
-            </button>
-          </div>
-        ) : (
-          <>
+        <li id={styles[showMenu ? "hidden" : ""]} className={styles.rightSide}>
+          {!!user ? (
             <div className={styles.container}>
-              <Link to="/register_pacient" className={styles.link}>
-                <button
-                  onClick={() => setMenu(!showMenu)}
-                  className={styles.register}
-                >
-                  PACIENTE
-                </button>
-              </Link>
+              <button
+                type="button"
+                className={styles.logoutBtn}
+                onClick={handleLogout}
+              >
+                CERRAR SESIÓN
+              </button>
             </div>
+          ) : (
+            <>
+              <div className={styles.container}>
+                <Link to="/register_pacient" className={styles.link}>
+                  <button id={styles["show"]} className={styles.register}>
+                    PACIENTE
+                  </button>
+                </Link>
+                <Link to="/register_pacient" className={styles.link}>
+                  <button id={styles["hidden"]} className={styles.register}>
+                    <img src={pacientLogo} alt="pacientLogo" />
+                  </button>
+                </Link>
+              </div>
 
-            <div className={styles.container}>
-              <Link to="/register_especialist" className={styles.link}>
-                <button
-                  onClick={() => setMenu(!showMenu)}
-                  className={styles.register}
-                >
-                  ESPECIALISTA
-                </button>
-              </Link>
-            </div>
+              <div className={styles.container}>
+                <Link to="/register_especialist" className={styles.link}>
+                  <button id={styles["show"]} className={styles.register}>
+                    ESPECIALISTA
+                  </button>
+                </Link>
+                <Link to="/register_especialist" className={styles.link}>
+                  <button id={styles["hidden"]} className={styles.register}>
+                    <img src={especialisLogo} alt="especialistLogo" />
+                  </button>
+                </Link>
+              </div>
 
-            <div className={styles.container}>
-              <Link to="/login" className={styles.link}>
-                <button
-                  onClick={() => setMenu(!showMenu)}
-                  className={styles.login}
-                >
-                  INICIAR SESIÓN
-                </button>
-              </Link>
-            </div>
-          </>
-        )}
-      </li>
-    </ul>
+              <div className={styles.container}>
+                <Link to="/login" className={styles.link}>
+                  <button id={styles["show"]} className={styles.login}>
+                    INICIAR SESIÓN
+                  </button>
+                </Link>
+                <Link to="/login" className={styles.link}>
+                  <button id={styles["hidden"]} className={styles.login}>
+                    <img src={loginLogo} alt="loginLogo" />
+                  </button>
+                </Link>
+              </div>
+              <div>
+                <Link to="/" className={styles.link}>
+                  <button id={styles["hidden"]} className={styles.login}>
+                    <img alt="homelogo" src={homelogo} />
+                  </button>
+                </Link>
+              </div>
+            </>
+          )}
+        </li>
+      </ul>
+    </div>
   );
 }
 
