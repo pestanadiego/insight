ab8263d52d72c83a632054a9128831ecca89a89f
samueloso <samuel.cedeno@correo.unimet.edu.ve>
2021-11-19 06:47:48 -0400

    dashboard terminado

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,18 +2,21 @@ import styles from "./Dashboard.module.css";
 import { useContext, useState } from "react";
 import { UserContext } from "../../context/UserContext";
 import { Link, useHistory } from "react-router-dom";
-import arrows from "../../images/icons/arrow.svg";
+import citas_image from "../../images/icons/dates.svg";
+import perfil_image from "../../images/icons/profile.svg";
+import pacientes_image from "../../images/icons/paciente.svg";
+import mensajes_image from "../../images/icons/mensaje.svg";
 
 function Dashboard() {
   const { user } = useContext(UserContext);
   const [showDash, setDash] = useState(false);
 
   return (
-    <div>
+    <div className={styles.maindiv}>
       {!!user ? (
-        <div id={styles[showDash ? "dash" : ""]} className={styles.dashboard}>
+        <div id={styles["dash"]} className={styles.dashboard}>
           <ul>
-            <li>
+            <li id={styles["showdash"]}>
               <Link
                 onClick={() => setDash(!showDash)}
                 to="/profile"
@@ -22,37 +25,40 @@ function Dashboard() {
                 Mi Perfil
               </Link>
             </li>
-            <li>
-              <Link
-                onClick={() => setDash(!showDash)}
-                to="/profile"
-                className={styles.link}
-              >
+            <li id={styles["hidden_d"]}>
+              <Link to="/profile" className={styles.link}>
+                <img src={perfil_image} alt="Mi perfil" />
+              </Link>
+            </li>
+            <li id={styles["showdash"]}>
+              <Link to="/profile" className={styles.link}>
                 Citas
               </Link>
             </li>
-            <li id={styles["arrow"]}>
-              <button onClick={() => setDash(!showDash)}>
-                {" "}
-                <img src={arrows} alt="arrows" />{" "}
-              </button>
+            <li id={styles["hidden_d"]}>
+              <Link to="/profile" className={styles.link}>
+                <img src={citas_image} alt="imagen_cita" />
+              </Link>
             </li>
-            <li>
-              <Link
-                onClick={() => setDash(!showDash)}
-                to="/profile"
-                className={styles.link}
-              >
+
+            <li id={styles["showdash"]}>
+              <Link to="/profile" className={styles.link}>
                 Pacientes
               </Link>
             </li>
-            <li>
-              <Link
-                onClick={() => setDash(!showDash)}
-                to="/profile"
-                className={styles.link}
-              >
+            <li id={styles["hidden_d"]}>
+              <Link to="/profile" className={styles.link}>
+                <img src={pacientes_image} alt="paciente_image" />
+              </Link>
+            </li>
+            <li id={styles["showdash"]}>
+              <Link to="/profile" className={styles.link}>
                 Mensajes
+              </Link>
+            </li>
+            <li id={styles["hidden_d"]}>
+              <Link to="/profile" className={styles.link}>
+                <img src={mensajes_image} alt="mensajes_image" />
               </Link>
             </li>
           </ul>
