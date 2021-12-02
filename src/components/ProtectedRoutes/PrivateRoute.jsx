61176fd821b591aeac3db9c37a3210ef9ba4630d
Mariano Moran <mariano.moran@correo.unimet.edu.ve>
2021-12-02 01:57:07 -0400

    Final

--- src/components/ProtectedRoutes/PrivateRoute.jsx
+++ src/components/ProtectedRoutes/PrivateRoute.jsx
@@ -4,11 +4,8 @@ import { UserContext } from "../../context/UserContext";
 
 const PrivateRoute = ({ component: View, ...args }) => {
   const { user, loading } = useContext(UserContext); // Para saber si hay un usuario logeado o no.
-  console.log("Usuario", user);
   const isLoggedIn = !!user;
   const isLoading = loading;
-  console.log("Logueado", isLoggedIn);
-  console.log("loading", isLoading);
 
   if (isLoading && !isLoggedIn) {
     return (
